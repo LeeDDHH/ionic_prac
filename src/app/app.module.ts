568737352281458314544b3c3f64e303cf8c687d
dongheon <niche3600@gmail.com>
2018-03-07 12:39:16 +0900

    use DatePipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+//LOCALE_ID：ユーザーが利用する言語や国・地域を扱う時に使う。
+import { ErrorHandler, NgModule, LOCALE_ID } from '@angular/core';
+//内部で使用されるグローバルのLocaleデータを登録する時に使う。
+import { registerLocaleData } from '@angular/common';
+//日本のlocaleデータを用いる時に使う。
+import localeja from '@angular/common/locales/ja'
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 //Web Storageを用いるために、IonicStorageModuleを組み込む
 import { IonicStorageModule } from '@ionic/storage';
@@ -11,6 +16,9 @@ import { HomePage } from '../pages/home/home';
 import { EventProvider } from '../providers/event/event';
 import { HttpClientModule } from '@angular/common/http';
 import { BookmarkProvider } from '../providers/bookmark/bookmark';
+
+//内部で使用されるグローバルのLocaleデータを登録する時に使う。
+registerLocaleData(localeja);
 
 @NgModule({
   declarations: [
@@ -33,6 +41,7 @@ import { BookmarkProvider } from '../providers/bookmark/bookmark';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: LOCALE_ID, useValue: navigator.language},
     EventProvider,
     BookmarkProvider
   ]
